9f3285b7d9d17250859c174fca15232fa3332ce7
hangzhouwh <hangzhouwh@gmail.com>
2021-04-10 17:21:00 +0800

    跨域

--- server/__init__.py
+++ server/__init__.py
@@ -23,7 +23,7 @@ def create_app(config_name=None):
     #     config_name = os.getenv('FLASK_CONFIG', 'development')
 
     app = Flask('server')
-    CORS(app, supports_credentials=True, resources=r'/*')
+    CORS(app, supports_credentials=True, resources=r'/*', methods=['GET', 'POST'])
     app.config.from_pyfile('settings.py')
 
     register_logging(app)  # 注册日志处理器
