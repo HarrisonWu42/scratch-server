573a53c0e629847feab04e472da982563debb307
hangzhouwh <hangzhouwh@gmail.com>
2021-05-11 16:56:20 +0800

    update

--- server/blueprints/admin.py
+++ server/blueprints/admin.py
@@ -18,45 +18,46 @@ admin_bp = Blueprint('admin', __name__)
 # 查看所有用户的权限
 @admin_bp.route('/users', methods=['GET'])
 def users():
-    users = User.query.all()
+	users = User.query.all()
 
-    json_array = []
-    for user in users:
-        user_obj = {"name": user.name,
-                    "email": user.email,
-                    "role": user.role.name}
-        json_array.append(user_obj)
-    json_dic = {"users": json_array}
+	json_array = []
+	for user in users:
+		user_obj = {"id": user.id,
+					"name": user.name,
+					"email": user.email,
+					"role": user.role.name}
+		json_array.append(user_obj)
+	json_dic = {"users": json_array}
 
-    return jsonify(code=200, data=json_dic)
+	return jsonify(code=200, data=json_dic)
 
 
 # 查询所有权限
 @admin_bp.route('/roles', methods=['GET'])
 def roles():
-    roles = Role.query.all()
+	roles = Role.query.all()
 
-    json_array = []
-    for role in roles:
-        role_obj = {"id": role.name,
-                    "name": role.email}
-        json_array.append(role_obj)
-    json_dic = {"roles": json_array}
+	json_array = []
+	for role in roles:
+		role_obj = {"id": role.name,
+					"name": role.email}
+		json_array.append(role_obj)
+	json_dic = {"roles": json_array}
 
-    return jsonify(code=200, data=json_dic)
+	return jsonify(code=200, data=json_dic)
 
 
 # 修改用户的权限
 @admin_bp.route('/edit-role/<user_id>', methods=['POST'])
 def edit_role(user_id):
-    form = EditRoleForm()
+	form = EditRoleForm()
 
-    role = Role.query.filter_by(name=form.role.data).first()
-    user = User.query.get(user_id)
-    user.role_id = role.id
-    db.session.commit()
+	role = Role.query.filter_by(name=form.role.data).first()
+	user = User.query.get(user_id)
+	user.role_id = role.id
+	db.session.commit()
 
-    return jsonify(code=200, message="Edit success", data={"id": user.id,
-                                                           "name": user.name,
-                                                           "email": user.email,
-                                                           "role": role.name})
+	return jsonify(code=200, message="Edit success", data={"id": user.id,
+														   "name": user.name,
+														   "email": user.email,
+														   "role": role.name})
